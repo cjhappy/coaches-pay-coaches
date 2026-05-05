129f96d88d11601ee3fa3860557d6897dccc3285
Christopher Happy <christopherhappy05@gmail.com>
2026-05-04 22:14:01 -0400

    fix supabase env vars

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://rjxzdryhcrpqxmrdfzmg.supabase.co'
-const SUPABASE_ANON_KEY = 'sb_publishable_Em4XEECEnpLAtOhF2pRQDw_3NIIT-E3'
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
