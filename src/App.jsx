77c0dd5b3792a812ed8a6f4be56135c349ad0b8a
Christopher Happy <christopherhappy05@gmail.com>
2026-05-05 11:59:08 -0400

    add coaches route

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Marketplace from './pages/Marketplace'
 import ListingDetail from './pages/ListingDetail'
 import Purchases from './pages/Purchases'
 import CoachProfile from './pages/CoachProfile'
+import Coaches from './pages/Coaches'
 import ResetPassword from './pages/ResetPassword'
 
 function App() {
@@ -25,6 +26,7 @@ function App() {
         <Route path="/marketplace" element={<ProtectedRoute><Marketplace /></ProtectedRoute>} />
         <Route path="/listing/:id" element={<ProtectedRoute><ListingDetail /></ProtectedRoute>} />
         <Route path="/coach/:id" element={<CoachProfile />} />
+        <Route path="/coaches" element={<Coaches />} />
         <Route path="/seller" element={<ProtectedRoute requiredRole="seller"><SellerDashboard /></ProtectedRoute>} />
         <Route path="/purchases" element={<ProtectedRoute><Purchases /></ProtectedRoute>} />
         <Route path="*" element={<Navigate to="/" replace />} />
